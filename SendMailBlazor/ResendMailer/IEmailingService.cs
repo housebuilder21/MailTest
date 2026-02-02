69c4178bb8012e98fd9630695b1bbd24b7202f52
housebuilder21 <dr.delphox@gmail.com>
2026-02-02 07:05:33 -0700

    Docs

--- SendMailBlazor/ResendMailer/IEmailingService.cs
+++ SendMailBlazor/ResendMailer/IEmailingService.cs
@@ -1,5 +1,9 @@
 ﻿namespace EmailService
 {
+    /// <summary>
+    /// An interface to blueprint what email services should do in this program. 
+    /// Use this to inject ResentMailer into the main program. (See line 17 in Program.cs)
+    /// </summary>
     public interface IEmailingService
     {
         public Task SendEmailAsync(EmailMessage emailMessage);
--- SendMailBlazor/SendMailBlazor/Components/Pages/Home.razor.cs
+++ SendMailBlazor/SendMailBlazor/Components/Pages/Home.razor.cs
@@ -6,7 +6,11 @@ namespace SendMailBlazor.Components.Pages
     public partial class Home
     {
         // Fields
-        const string _SENDER_ADDRESS = "tester@testing.worrynotsmp.xyz"; // Namecheap domain I had lying around - housebuilder21
+        /*
+            It is important to have subdomains for different areas of 
+         */
+        private const string _SENDER_ADDRESS = "tester@testing.worrynotsmp.xyz"; // Namecheap domain I had lying around - housebuilder21
+        
         string recipient = string.Empty;
         string message = string.Empty;
         bool awaitingMessage = false;
@@ -26,14 +30,21 @@ namespace SendMailBlazor.Components.Pages
         {
             try
             {
-                //string fullMessage = $"<p><strong>Hello there!</strong></p>\n<p>This email is a test message sent from a Blazor app using Resend's email API. The sender has an additional message below:</p>\n{message}";
-                string messageDocument = "Components/EmailTemplates/test-html.html"; //File.ReadAllText("Components/EmailTemplates/htmlpage.html");
+                // See Components/EmailTemplates/test-html.html to see the message itsel.
+                string messageDocument = "Components/EmailTemplates/test-html.html";
+                /*
+                    NOTICE: It is important to add a text version of the email you're sending. It builds domain trust (less likely for
+                    the email to end up in the spam folder) and ensures that the message is readable if the recipient's email app
+                    cannot view HTML versions of email.
+                */
                 string textVersion = $"Hello there!\nThis email is a test message sent from a Blazor app using Resend's email API. The sender has an additional message below:\n{message}";
                 awaitingMessage = true;
 
+                // The EmailMessage class has two constructors. One for manually putting in the HTML,
+                // and this one to utilize an HTML file for a template.
                 await EmailService.SendEmailAsync(new EmailMessage(
                     recipient, // To
-                    _SENDER_ADDRESS, // From
+                    _SENDER_ADDRESS, // From - See lines 9 - * for important information.
                     "Test Mail", // Subject
                     textVersion, // Plain Text Version
                     messageDocument, // Filepath to Template
